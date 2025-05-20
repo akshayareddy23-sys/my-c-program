0adee270a77add3adb61325a44e9ac37be65e37c
akshayareddy23-sys <akshaya.yardly@gmail.com>
2025-05-20 21:59:48 +0530

    equality of number .c

---
+++ equality of number .c
@@ -0,0 +1,25 @@
+int main()
+{
+    int num1,num2;
+    printf("enter the first number");
+    scanf("%d",&num1);
+    printf("enter the second numbers");
+    scanf("%d",&num2);
+    if (num1==num2){
+    printf("the numbers are equal");
+    } else{
+        printf("the numbers are not equal");
+    }  
+    }
+    
+    out put
+enter the first number4
+enter the second numbers6
+the numbers are not equal
+
+=== Code Execution Successful ===
+    
+
+    
+
+
