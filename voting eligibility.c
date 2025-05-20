96a271606f94a8e097dee0a8e9954518de15dd04
akshayareddy23-sys <akshaya.yardly@gmail.com>
2025-05-20 22:09:29 +0530

    voting eligibility.c

---
+++ voting eligibility.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+    printf("enter the age");
+    scanf("%d",&age);
+    if(age>=18)
+    printf("the person is eligible to vote");
+    else if (age<18)
+    printf("the person is not eligible to vote");
+}
+out put
+enter the age15
+the person is not eligible to vote
+
+=== Code Execution Successful ===
+
+out put
+enter the age21
+the person is eligible to vote
+
+=== Code Execution Successful ===
