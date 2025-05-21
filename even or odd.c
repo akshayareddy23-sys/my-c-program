440bf3d7f40bafb0a831d812af4da3a05b162c30
akshayareddy23-sys <akshaya.yardly@gmail.com>
2025-05-21 22:36:05 +0530

    even or odd.c

---
+++ even or odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("enter the number");
+    scanf("%d",&num);
+    if(num%2==0)
+    printf("the number is even");
+    else
+    printf("the number is odd");
+}
+out put
+enter the number64
+the number is even
+
+=== Code Execution Successful ===
